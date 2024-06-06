0da7ef18e689232dceba53ef161b992ef74c233e
Mario Juarez <mario@mjp.one>
2024-06-06 23:22:00 +0200

    Show question description

--- resources/views/questions/chart-question.blade.php
+++ resources/views/questions/chart-question.blade.php
@@ -6,6 +6,7 @@
     $description = $this->getDescription();
 
     $hasHeading = filled($heading);
+    $hasDescription = filled((string) $description);
 @endphp
 
 <div
@@ -15,12 +16,19 @@
     class="adffi-chart-question h-full flex flex-col p-4 rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"
 >
     @if($hasHeading)
-    <h3
-        @class(['mb-4 text-base font-semibold leading-6 text-gray-950 dark:text-white'])
-    >
-        {{ $heading }}
-    </h3>
+        <h3
+            @class([$hasDescription ? 'mb-1' : 'mb-4', 'text-base font-semibold leading-6 text-gray-950 dark:text-white'])
+        >
+            {{ $heading }}
+        </h3>
     @endif
+        @if ($hasDescription)
+            <p
+                @class(['mb-4 overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400'])
+            >
+                {{ $description }}
+            </p>
+        @endif
     <div
         @if (FilamentView::hasSpaMode())
             ax-load="visible"
