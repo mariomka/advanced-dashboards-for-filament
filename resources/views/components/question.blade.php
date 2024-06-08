06965bbb97fdaddce1494f701a97df602a217fc9
Mario Juarez <mario@mjp.one>
2024-06-08 22:49:08 +0200

    Update paddings and add a separator between header and content

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -11,24 +11,28 @@
 <div
     {{
         $attributes
-            ->class(["adffi-question h-full flex flex-col p-4 rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"])
+            ->class(["adffi-question h-full flex flex-col rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10"])
     }}
 >
-    @if($hasHeading)
-        <h3
-            @class([$hasDescription ? 'mb-1' : 'mb-4', 'text-base font-semibold leading-6 text-gray-950 dark:text-white'])
-        >
-            {{ $heading }}
-        </h3>
+    @if($hasHeading || $hasDescription)
+        <header class="px-6 py-4 border-b border-gray-200 dark:border-white/10">
+            @if($hasHeading)
+                <h3
+                    @class([$hasDescription ? 'mb-1' : '', 'text-base font-semibold leading-6 text-gray-950 dark:text-white'])
+                >
+                    {{ $heading }}
+                </h3>
+            @endif
+            @if ($hasDescription)
+                <p
+                    @class(['overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400'])
+                >
+                    {{ $description }}
+                </p>
+            @endif
+        </header>
     @endif
-    @if ($hasDescription)
-        <p
-            @class(['mb-4 overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400'])
-        >
-            {{ $description }}
-        </p>
-    @endif
-    <div class="adffi-question-content">
+    <div class="adffi-question-content px-4 py-6">
         {{ $slot }}
     </div>
 </div>
