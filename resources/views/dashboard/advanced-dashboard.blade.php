8dd04e0b12732a5217a215a45f04ac9f630a4921
Mario Juarez <mario@mjp.one>
2024-06-09 09:02:55 +0200

    Set max width when enters on fullscreen and fix scroll

--- resources/views/dashboard/advanced-dashboard.blade.php
+++ resources/views/dashboard/advanced-dashboard.blade.php
@@ -1,7 +1,7 @@
 <div
     x-ref="advancedDashboard"
-    :class="isFullscreen ? 'px-4 md:px-6 lg:px-8' : ''"
     class="adffi-advanced-dashboard-page"
+    :class="isFullscreen ? 'w-full h-full overflow-y-auto' : ''"
     x-data="{ isFullscreen: false }"
     @fullscreenchange.window="isFullscreen = (document.fullscreenElement === $refs.advancedDashboard)"
     @if($loadCss)
@@ -11,23 +11,25 @@
         wire:poll.{{ $pollingInterval }}="$refresh"
     @endif
 >
-    <x-filament-panels::page>
-        @if ($this->showFiltersForm())
-            <form wire:submit="$refresh">
-                {{ $this->filtersForm }}
-            </form>
-        @endif
+    <div :class="isFullscreen ? 'w-full m-auto max-w-[1920px] px-4 md:px-6 lg:px-8' : ''">
+        <x-filament-panels::page>
+            @if ($this->showFiltersForm())
+                <form wire:submit="$refresh">
+                    {{ $this->filtersForm }}
+                </form>
+            @endif
 
-        <x-advanced-dashboards-for-filament::questions-grid
-            :columns="$this->getColumns()"
-            :rowHeight="$this->getRowHeight()"
-            :data="
+            <x-advanced-dashboards-for-filament::questions-grid
+                :columns="$this->getColumns()"
+                :rowHeight="$this->getRowHeight()"
+                :data="
                 [
                     ...(property_exists($this, 'filters') ? ['filters' => $this->filters] : []),
                     ...$this->getWidgetData(),
                 ]
             "
-            :questions="$this->getQuestions()"
-        />
-    </x-filament-panels::page>
+                :questions="$this->getQuestions()"
+            />
+        </x-filament-panels::page>
+    </div>
 </div>
